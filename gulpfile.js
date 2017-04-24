1a7ec688ea4881f61d7220c7d7e914c6387cadc3
Flaki <git@flaki.hu>
2017-04-24 16:15:01 +0200

    Concat polyfills, Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 
-gulp.task('default', function() {
+const polyfills = [
+  'node_modules/promise-polyfill/promise.min.js',
+  'node_modules/whatwg-fetch/fetch.js',
+  'node_modules/jquery/dist/jquery.min.js'
+]
+
+gulp.task('js-polyfills', function() {
+  gulp.src(polyfills)
+    .pipe(concat('libs.min.js'))
+    .pipe(gulp.dest('prod/js'))
+});
+
+gulp.task('js-app', function() {
   gulp.src([ 'js/*api.js', 'js/app.js'])
     .pipe(concat('app.min.js'))
     .pipe(gulp.dest('prod/js'))
 });
+
+gulp.task('default', [ 'js-polyfills', 'js-app' ]);
