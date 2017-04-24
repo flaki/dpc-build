796f8104900c5a316d6fcbcac80495732fd78708
Flaki <git@flaki.hu>
2017-04-24 16:47:36 +0200

    ESLint Config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "parserOptions": {
+           "ecmaVersion": 6,
+           "sourceType": "module"
+    },
+    "rules": {
+        "curly": 2,
+        "no-undef": 0,
+        "no-console": 1,
+    }
+};
